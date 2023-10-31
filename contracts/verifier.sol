1f2fefc81735e5500ff13788e6b3d1d2536ed123
Somesh Banerjee <54861461+somesh-banerjee@users.noreply.github.com>
2023-10-31 17:34:20 +0530

    add contract and contract test

---
+++ contracts/verifier.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+    Copyright 2021 0KIMS association.
+
+    This file is generated with [snarkJS](https://github.com/iden3/snarkjs).
+
+    snarkJS is a free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    snarkJS is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+    License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with snarkJS. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+pragma solidity 0.8.19;
+
+contract Groth16Verifier {
+    // Scalar field size
+    uint256 constant r    = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
+    // Base field size
+    uint256 constant q   = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+
+    // Verification Key data
+    uint256 constant alphax  = 20574036300207438978793335128134878840256323922098895046100367048301770235301;
+    uint256 constant alphay  = 3570361468187177998845895411188517453032884125793192445858593042470521904591;
+    uint256 constant betax1  = 15357790948299761391385571357749967395271839566758198144292193314730928573868;
+    uint256 constant betax2  = 12227753414027259248794038269939794673672800393517368229076225621024085091808;
+    uint256 constant betay1  = 13504148701019203474450267636786387128269095549017513193161385002738716088372;
+    uint256 constant betay2  = 20736238685192181961971814616342959819459483209826631766580187517860974497695;
+    uint256 constant gammax1 = 11559732032986387107991004021392285783925812861821192530917403151452391805634;
+    uint256 constant gammax2 = 10857046999023057135944570762232829481370756359578518086990519993285655852781;
+    uint256 constant gammay1 = 4082367875863433681332203403145435568316851327593401208105741076214120093531;
+    uint256 constant gammay2 = 8495653923123431417604973247489272438418190587263600148770280649306958101930;
+    uint256 constant deltax1 = 701992387410083389949495019057640213047907015820093578156656383352422031052;
+    uint256 constant deltax2 = 15876494045288245714747159869847223244190917454838199225674503011087981437169;
+    uint256 constant deltay1 = 14136191061314216957219456936683654434248735217467948372542981252390283393266;
+    uint256 constant deltay2 = 14281746083708685169210772660189440290018388798710146150759500590199814380923;
+
+    
+    uint256 constant IC0x = 18471845083106170031638769896741443827672508869451476997207298427152108480103;
+    uint256 constant IC0y = 16508639150189015222006646284814368811917941461924978646961030823462245118945;
+    
+    uint256 constant IC1x = 11993513990781967739816384819869839498634151524989153841330866215376937371683;
+    uint256 constant IC1y = 2110978560129138993609107010064389861396002505882788654368079049423617886058;
+    
+ 
+    // Memory data
+    uint16 constant pVk = 0;
+    uint16 constant pPairing = 128;
+
+    uint16 constant pLastMem = 896;
+
+    function verifyProof(uint[2] calldata _pA, uint[2][2] calldata _pB, uint[2] calldata _pC, uint[1] calldata _pubSignals) public view returns (bool) {
+        assembly {
+            function checkField(v) {
+                if iszero(lt(v, q)) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+            
+            // G1 function to multiply a G1 value(x,y) to value in an address
+            function g1_mulAccC(pR, x, y, s) {
+                let success
+                let mIn := mload(0x40)
+                mstore(mIn, x)
+                mstore(add(mIn, 32), y)
+                mstore(add(mIn, 64), s)
+
+                success := staticcall(sub(gas(), 2000), 7, mIn, 96, mIn, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+
+                mstore(add(mIn, 64), mload(pR))
+                mstore(add(mIn, 96), mload(add(pR, 32)))
+
+                success := staticcall(sub(gas(), 2000), 6, mIn, 128, pR, 64)
+
+                if iszero(success) {
+                    mstore(0, 0)
+                    return(0, 0x20)
+                }
+            }
+
+            function checkPairing(pA, pB, pC, pubSignals, pMem) -> isOk {
+                let _pPairing := add(pMem, pPairing)
+                let _pVk := add(pMem, pVk)
+
+                mstore(_pVk, IC0x)
+                mstore(add(_pVk, 32), IC0y)
+
+                // Compute the linear combination vk_x
+                
+                g1_mulAccC(_pVk, IC1x, IC1y, calldataload(add(pubSignals, 0)))
+                
+
+                // -A
+                mstore(_pPairing, calldataload(pA))
+                mstore(add(_pPairing, 32), mod(sub(q, calldataload(add(pA, 32))), q))
+
+                // B
+                mstore(add(_pPairing, 64), calldataload(pB))
+                mstore(add(_pPairing, 96), calldataload(add(pB, 32)))
+                mstore(add(_pPairing, 128), calldataload(add(pB, 64)))
+                mstore(add(_pPairing, 160), calldataload(add(pB, 96)))
+
+                // alpha1
+                mstore(add(_pPairing, 192), alphax)
+                mstore(add(_pPairing, 224), alphay)
+
+                // beta2
+                mstore(add(_pPairing, 256), betax1)
+                mstore(add(_pPairing, 288), betax2)
+                mstore(add(_pPairing, 320), betay1)
+                mstore(add(_pPairing, 352), betay2)
+
+                // vk_x
+                mstore(add(_pPairing, 384), mload(add(pMem, pVk)))
+                mstore(add(_pPairing, 416), mload(add(pMem, add(pVk, 32))))
+
+
+                // gamma2
+                mstore(add(_pPairing, 448), gammax1)
+                mstore(add(_pPairing, 480), gammax2)
+                mstore(add(_pPairing, 512), gammay1)
+                mstore(add(_pPairing, 544), gammay2)
+
+                // C
+                mstore(add(_pPairing, 576), calldataload(pC))
+                mstore(add(_pPairing, 608), calldataload(add(pC, 32)))
+
+                // delta2
+                mstore(add(_pPairing, 640), deltax1)
+                mstore(add(_pPairing, 672), deltax2)
+                mstore(add(_pPairing, 704), deltay1)
+                mstore(add(_pPairing, 736), deltay2)
+
+
+                let success := staticcall(sub(gas(), 2000), 8, _pPairing, 768, _pPairing, 0x20)
+
+                isOk := and(success, mload(_pPairing))
+            }
+
+            let pMem := mload(0x40)
+            mstore(0x40, add(pMem, pLastMem))
+
+            // Validate that all evaluations ∈ F
+            
+            checkField(calldataload(add(_pubSignals, 0)))
+            
+            checkField(calldataload(add(_pubSignals, 32)))
+            
+
+            // Validate all evaluations
+            let isValid := checkPairing(_pA, _pB, _pC, _pubSignals, pMem)
+
+            mstore(0, isValid)
+             return(0, 0x20)
+         }
+     }
+ }
---
+++ test/age_ab18.test.ts
@@ -0,0 +1,58 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const { groth16 } = require('snarkjs')
+
+let verifier : any
+const wasmFilePath = './zk-codes/build/age_ab18_js/age_ab18.wasm'
+const finalZkeyPath = './zk-codes/build/snarkjs/age_ab18_0001.zkey'
+
+describe('AgeVerifier', () => {
+  before(async () => {
+    const Verifier = await ethers.getContractFactory('Groth16Verifier')
+
+    verifier = await Verifier.deploy()
+  })
+
+  it('Should return true if age is above 18', async () => {
+    const witness = {
+      age: 22
+    }
+
+    const { proof, publicSignals } = await groth16.fullProve(witness, wasmFilePath, finalZkeyPath, null)
+
+    const transaction = await verifier.verifyProof(
+        [proof.pi_a[0], proof.pi_a[1],], 
+        [
+        [proof.pi_b[0][1], proof.pi_b[0][0],],
+        [proof.pi_b[1][1], proof.pi_b[1][0],]
+      ],
+      [proof.pi_c[0], proof.pi_c[1]],
+        publicSignals)
+        
+    expect(transaction).to.be.equal(true)
+  })
+
+  it('Should return false if age is below 18', async () => {
+    const witness = {
+      age: 16
+    }
+
+    const { proof, publicSignals } = await groth16.fullProve(witness, wasmFilePath, finalZkeyPath, null)
+
+    try {
+        const transaction = verifier.verifyProof(
+            [proof.pi_a[0], proof.pi_a[1],], 
+            [
+            [proof.pi_b[0][1], proof.pi_b[0][0],],
+            [proof.pi_b[1][1], proof.pi_b[1][0],]
+          ],
+          [proof.pi_c[0], proof.pi_c[1]],
+            publicSignals
+        )
+    } catch (error) {
+        
+    }
+        
+
+  })
+})
