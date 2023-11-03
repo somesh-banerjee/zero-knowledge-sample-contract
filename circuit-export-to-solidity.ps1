b5c08ba025a052900c9c3bf588e44f16fd8e95fe
Somesh Banerjee <54861461+somesh-banerjee@users.noreply.github.com>
2023-11-03 18:28:50 +0530

    Create circuit-export-to-solidity.ps1

---
+++ circuit-export-to-solidity.ps1
@@ -0,0 +1,4 @@
+node .\zk-codes\build\age_ab18_js\generate_witness.js .\zk-codes\build\age_ab18_js\age_ab18.wasm .\zk-codes\build\age_ab18_js\input.json .\zk-codes\build\age_ab18_js\witness.wtns
+snarkjs groth16 setup .\zk-codes\build\age_ab18.r1cs .\zk-codes\build\snarkjs\pot12_final.ptau .\zk-codes\build\snarkjs\age_ab18_0000.zkey  
+snarkjs zkey contribute .\zk-codes\build\snarkjs\age_ab18_0000.zkey .\zk-codes\build\snarkjs\age_ab18_0001.zkey --name="Somesh" -v    
+snarkjs zkey export solidityverifier .\zk-codes\build\snarkjs\age_ab18_0001.zkey .\contracts\verifier.sol          
